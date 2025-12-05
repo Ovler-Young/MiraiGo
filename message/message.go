798f901b668b3c623f9a9b04c95c70fdfc57e3fa
Ovler <ovlertheyoung@gmail.com>
2025-12-05 05:31:44 -0500

    Revert "feat: face also use fileId"
    This reverts commit 5c0e022a99bb95f97e28834821af86435bce450f.

--- message/message.go
+++ message/message.go
@@ -521,23 +521,14 @@ func ParseMessageElems(elems []*msg.Elem) []IMessageElement {
 			if len(elem.CustomFace.Md5) == 0 {
 				continue
 			}
-			// DEBUG: Log entire elem (not just CustomFace)
+
 			fmt.Printf("[DEBUG elem] %+v\n", elem)
 
-			// Build URL using new download API format with FileId
-			fileId := elem.CustomFace.FileId.Unwrap()
 			var url string
-			if fileId != 0 {
-				url = fmt.Sprintf("https://gchat.qpic.cn/download?appid=1407&fileid=%d", fileId)
-				if GetRKey != nil {
-					if rkey := GetRKey(true); rkey != "" {
-						url += "&rkey=" + rkey
-					}
-				}
-			} else if elem.CustomFace.OrigUrl.Unwrap() != "" {
-				url = "https://gchat.qpic.cn" + elem.CustomFace.OrigUrl.Unwrap()
-			} else {
+			if elem.CustomFace.OrigUrl.Unwrap() == "" {
 				url = fmt.Sprintf("https://gchat.qpic.cn/gchatpic_new/0/0-0-%X/0?term=2", elem.CustomFace.Md5)
+			} else {
+				url = "https://gchat.qpic.cn" + elem.CustomFace.OrigUrl.Unwrap()
 			}
 			if strings.Contains(elem.CustomFace.OrigUrl.Unwrap(), "qmeet") {
 				res = append(res, &GuildImageElement{
